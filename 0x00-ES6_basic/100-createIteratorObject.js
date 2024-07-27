d170a61daa1c4829257cfdb33209dd941972aaf9
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-07-27 16:38:57 +0100

    task 100 done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,17 +1,9 @@
 export default function createIteratorObject(report) {
   let employees = [];
-  for (const department in report.allEmployees) {
-    employees = employees.concat(report.allEmployees[department]);
+
+  for (const department of Object.values(report.allEmployees)) {
+    employees = employees.concat(department);
   }
-  return {
-    next() {
-      if (employees.length === 0) {
-        return { done: true };
-      }
-      return { value: employees.shift(), done: false };
-    },
-    [Symbol.iterator]() {
-      return this;
-    },
-  };
+
+  return employees[Symbol.iterator]();
 }

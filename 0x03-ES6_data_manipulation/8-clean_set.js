d5e064aa0e77294a8c777468a74b8fac335f09cd
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-08-03 21:19:40 +0100

    task 8 done

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return '';
+
+  return [...set].filter((item) => item.startsWith(startString))
+    .map((item) => item.slice(startString.length))
+    .join('-');
+}

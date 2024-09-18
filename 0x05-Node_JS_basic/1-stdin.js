64ee13a271d37c6c0ece39ba54564a7490ee017c
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-18 16:31:15 +0100

    task 1 done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,9 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const inputName = data.toString().trim();
-  console.log(`Your name is: ${inputName}`);
+  process.stdout.write(`Your name is: ${inputName}`);
 
-  process.stdout.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 
   process.exit();
 });

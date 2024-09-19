6cb734fca7423247896539aaf17e68d72810f79a
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-19 15:27:18 +0100

    task 7 done

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,34 +1,39 @@
-const express = require('express');
-const countStudents = require('./3-read_file_async');
-
+const express = require("express");
+const students = require("./3-read_file_async");
 const app = express();
+const hostname = "127.0.0.1";
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+app.get("/", (req, res) => {
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/plain");
+	res.send("Hello Holberton School!");
 });
 
-app.get('/students', async (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  try {
-    const data = await countStudents(process.argv[2]);
-    res.write(`Number of students: ${data.total}\n`);
-    for (const [field, students] of Object.entries(data.fields)) {
-      res.write(
-        `Number of students in ${field}: ${
-          students.length
-        }. List: ${students.join(', ')}\n`,
-      );
-    }
-    res.end();
-  } catch (error) {
-    res.end(error.message);
-  }
+app.get("/students", async (req, res) => {
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/plain");
+	res.write("This is the list of our students\n");
+	await students(process.argv[2])
+		.then((data) => {
+			res.write(`Number of students: ${data.students.length}\n`);
+			res.write(
+				`Number of students in CS: ${
+					data.csStudents.length
+				}. List: ${data.csStudents.join(", ")}\n`
+			);
+			res.write(
+				`Number of students in SWE: ${
+					data.sweStudents.length
+				}. List: ${data.sweStudents.join(", ")}`
+			);
+		})
+		.catch((err) => res.write(err.message))
+		.finally(() => {
+			res.end();
+		});
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}`);
 });
-
-module.exports = app;

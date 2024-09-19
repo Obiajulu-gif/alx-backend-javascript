dd9670f5c853b9de78dce35d2d00a5ff161cc6d6
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-19 15:21:47 +0100

    task 7 done

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.write(`Number of students: ${data.total}\n`);
+    for (const [field, students] of Object.entries(data.field)) {
+      res.write(
+        `Number of students in ${field}: ${
+          students.length
+        }. List: ${students.join(', ')}\n`,
+      );
+    }
+  } catch (error) {
+    res.end(error.message);
+  }
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;

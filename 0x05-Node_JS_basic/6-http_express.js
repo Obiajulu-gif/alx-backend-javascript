803f8ad239ff86430625887ac5d7824cde931ce1
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-19 15:01:45 +0100

    task 6 done

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,16 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const port = 1245;
 
-app.get("/", (req, res) => {
-	res.send("Hello Holberton School!");
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
 
 module.exports = app;
